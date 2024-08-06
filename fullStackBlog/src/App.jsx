e5b018115e451ec67a5520087a791f01fc518a39
MilanPavasiya <milpavasiya@gmail.com>
2024-08-06 19:35:52 -0400

    configured store: redux-toolkit

--- fullStackBlog/src/App.jsx
+++ fullStackBlog/src/App.jsx
@@ -1,13 +1,39 @@
 import './App.css';
+import React, { useState, useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import authService from './appwrite/auth';
+import { login, logout } from './store/authslice';
+import { Header, Footer } from './components';
+import { Outlet } from 'react-router-dom';
 
 function App() {
-	console.log(import.meta.env.VITE_APPWRITE_URL);
+	const [loading, setLoading] = useState(true);
+	const dispatch = useDispatch();
 
-	return (
-		<>
-			<h1>A blog with Appwrite</h1>
-		</>
-	);
+	useEffect(() => {
+		authService
+			.getCurrentUser()
+			.then((userData) => {
+				if (userData) {
+					dispatch(login({ userData }));
+				} else {
+					dispatch(logout());
+				}
+			})
+			.catch((e) => console.log('Error :: App-useEffect', e))
+			.finally(() => setLoading(false));
+	}, []);
+
+	return !loading ? (
+		<div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+			<div className='w-full block'></div>
+			<Header />
+			<main>
+				TODO: <outlet />
+			</main>
+			<Footer />
+		</div>
+	) : null;
 }
 
 export default App;
