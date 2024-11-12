9525fea3613f12d0b859e010365697b9c8cc4347
MilanPavasiya <milpavasiya@gmail.com>
2024-11-11 19:26:25 -0500

    Login component

---
+++ fullStackBlog/src/components/Login.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { login, authLogin } from '../store/authslice';
+
+function Login() {
+	return <div>Login</div>;
+}
+
+export default Login;
